ce6e2c39fcda7dfc96b7d359093086d92e02d67f
tgdriro5 <ronald.rink@d-fens.net>
2015-01-20 12:39:35 +0100

    2015-01-20; rrink; CHG: resolves #5

--- Enter-vCenter.ps1
+++ Enter-vCenter.ps1
@@ -1,87 +1,261 @@
 function Enter-vCenter {
 <#
 .SYNOPSIS
-
-Connect to the required vCenter Servers
+Connects to a VMware vCenter server.
 
 
 .DESCRIPTION
+Connects to a VMware vCenter server.
 
-Helper Function - Connect to the required vCenter Servers
 
+.EXAMPLE
+Enter-vCenter https://vcenter1
+
+Connects to vCenter1 via https
+
+.EXAMPLE
+Enter-vCenter vcenter1
+
+Connects to vCenter1 via https and tcp port 443 (implicit)
+
+.EXAMPLE
+Enter-vCenter -Server vcenter1 -Port 44300 -Scheme https
+
+Connects to vCenter1 via https and tcp port 44300
+
+.EXAMPLE
+Enter-vCenter @('vcenter1', 'vcenter2')
+
+Connects to the list of vCenters vcenter1 and vcenter2
+
+.EXAMPLE
+Enter-vCenter 'vcenter1','vcenter2'
+
+Connects to the list of vCenters vcenter1 and vcenter2
+
+.EXAMPLE
+@('vcenter1', 'vcenter2') Enter-vCenter
+
+Connects to the list of vCenters vcenter1 and vcenter2 via pipeline input
+
+.EXAMPLE
+'vcenter1','vcenter2' | Enter-vCenter
+
+Connects to the list of vCenters vcenter1 and vcenter2 via pipeline input
+
+.EXAMPLE
+Enter-vCenter https://vcenter1 -Force
+
+Will login to a vCenter even if a connection already exists (existing connection will be disconnecte first)
+
+.EXAMPLE
+Enter-vCenter vcenter1 -Credential $cred
+
+Connects to vCenter1 with explicit Credentials ($cred)
 
 .NOTES
-
 See module manifest for required software versions and dependencies at:
 http://dfch.biz/biz/dfch/PS/Cumulus/VI/biz.dfch.PS.Cumulus.VI.psd1/
 
-
+Aliases 'vCenterNames' and 'vCenterCred' only left for compability. 
+Should be removed at a future point in time.
 #>
-
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "Medium"
+	,
+	HelpURI = 'http://dfch.biz/biz/dfch/PS/Cumulus/VI/Enter-vCenter/'
+	,
+	DefaultParameterSetName = 'uri'
+)]
 PARAM
 (
 	# Specifies the vCenter names to connect to
-	[Parameter(Mandatory = $true, Position = 0)]
+	[ValidateScript( { foreach($Object in $_) { if( $Object -isnot [String] -And $Object -isnot [Uri] ) { throw($Object); }; return $true; }; } )]
+	[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'input')]
 	[ValidateNotNullOrEmpty()]
 	[Alias('vCenterNames')]
 	$InputObject
 	,
+	# Specifies the vCenter names to connect to
+	[Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'uri')]
+	[ValidateNotNullOrEmpty()]
+	[Uri] $Uri
+	,
+	# Specifies the vCenter names to connect to
+	[Parameter(Mandatory = $true, ParameterSetName = 'server')]
+	[ValidateNotNullOrEmpty()]
+	[String] $Server
+	,
+	# Specifies the vCenter port number to connect to
+	[Parameter(Mandatory = $true, ParameterSetName = 'server')]
+	[ValidateNotNullOrEmpty()]
+	[int] $Port = 443
+	,
+	# Specifies the vCenter protocol scheme to connect to
+	[Parameter(Mandatory = $true, ParameterSetName = 'server')]
+	[ValidateSet('http', 'https')]
+	[String] $Scheme = 'https'
+	,
 	# Specifies the credential set to make the connection to the vCenter
-	[Parameter(Mandatory = $false, Position = 1, ParameterSetName = 'cred')]
+	[Parameter(Mandatory = $false, Position = 1)]
 	[ValidateNotNullOrEmpty()]
 	[Alias('vCenterCred')]
 	[PSCredential] $Credential
+	,
+	# Specifies whether an existing conenction to the vCenter server should be overwritten (and disconnected first)
+	[Parameter(Mandatory = $false)]
+	[Switch] $Force = $false
 )
 BEGIN
 {
 	$datBegin = [datetime]::Now;
 	[string] $fn = $MyInvocation.MyCommand.Name;
-	Log-Debug -fn $fn -msg ("CALL");
+	Log-Debug -fn $fn -msg ("CALL. ParameterSetName '{0}'" -f $PSCmdlet.ParameterSetName) -fac 1;
 	
-	# DFCHECK - what is this? and what for ?
-	## check to see if there are any currently connected servers
-	if ($global:DefaultVIServers.Count -gt 0) {
-		
-		foreach( $VIServer in $global:DefaultVIServers)
+	if('server' -eq $PSCmdlet.ParameterSetName)
+	{
+		$UriString = '{0}://{1}:{2}' -f $Scheme, $Server, $Port;
+		if(![Uri]::TryCreate($UriString, [System.UriKind]::Absolute, [ref] $Uri))
 		{
-			###TODO###
+			$msg = "Scheme/Server/Port: Parameter validation FAILED."; 
+			Log-Error $fn $msg;
+			$e = New-CustomErrorRecord -m $msg -cat InvalidArgument -o $PSBoundParameters; 
+			throw($gotoError); 
+			$PSCmdlet.ThrowTerminatingError($e);
 		}
-	} 
+	}
+	if($Uri)
+	{
+		$InputObject = $Uri;
+	}
 }
 # BEGIN
 
 PROCESS
 {
 
+[Boolean] $fReturn = $false;
+$OutputParameter = $null;
+
 try
 {
 	foreach($Object in $InputObject)
 	{
-		# DFCHECK error checking
-		Log-Debug -fn $fn -msg ("Connecting to vCenter '{0}' ..." -f $Object );
+		if(!$PSCmdlet.ShouldProcess(($Object)))
+		{
+			continue;
+		}
+		if($Object -is [String])
+		{
+			[Uri] $LocalUri = $null;
+			if(![Uri]::TryCreate($Object, [System.UriKind]::Absolute, [ref] $LocalUri))
+			{
+				$Object = '{0}://{1}:{2}' -f $Scheme, $Object, $Port;
+				if(![Uri]::TryCreate($Object, [System.UriKind]::Absolute, [ref] $LocalUri))
+				{
+					$msg = "Object: Parameter validation FAILED."; 
+					Log-Error $fn $msg;
+					$e = New-CustomErrorRecord -m $msg -cat InvalidArgument -o $Object; 
+					throw($gotoError); 
+					$PSCmdlet.ThrowTerminatingError($e);
+				}
+			}
+			$Object = $LocalUri;
+		}
+		
+		foreach($VIServer in $global:DefaultVIServers)
+		{
+			# this check will produce a 'false negative' if host only and FQDN are compared
+			[Uri] $UriVIServer = $VIServer.ServiceUri;
+			if($Object.AbsoluteUri -eq $UriVIServer.AbsoluteUri)
+			{
+				if(!$Force)
+				{
+					Log-Warn $fn ("ViServer '{0}' == '{1}' is already connected." -f $Object.AbsoluteUri, $ViServer.ServiceUri);
+					continue;
+				}
+				Log-Info $fn ("Disconnecting from already connected ViServer '{0}' (Force) ..." -f $ViServer.ServiceUri);
+				$null = Disconnect-VIServer -Server $VIServer -Confirm:$false -Force -ErrorAction:SilentlyContinue;
+				break;
+			}
+		}
+		Log-Debug $fn ("Connecting to ViServer '{0}' ..." -f $Object.AbsoluteUri);
 		if($Credential)
 		{
-			$OutputParameter = Connect-VIServer -Server $Object -Credential $Credential;
-		} 
-		else 
-		{
-			$OutputParameter = Connect-VIServer -Server $Object;
+			$ViServerConnection = Connect-VIServer -Server $Object.Host -Port $Object.Port -Protocol $Object.Scheme -Credential $Credential -ErrorAction:SilentlyContinue -Force;
 		}
-		$OutputParameter;
+		else
+		{
+			$ViServerConnection = Connect-VIServer -Server $Object.Host -Port $Object.Port -Protocol $Object.Scheme -ErrorAction:SilentlyContinue -Force;
+		}
+		if(!$ViServerConnection)
+		{
+			Log-Error $fn ("Connecting to ViServer '{0}' FAILED." -f $Object.AbsoluteUri);
+			continue;
+		}
+		$OutputParameter += $ViServerConnection;
+		Log-Info $fn ("Connecting to ViServer '{0}' SUCCEEDED." -f $Object.AbsoluteUri);
 	}
 }
-catch
+catch 
 {
-	# DFECHECK - correct throw/catch
-	Log-Error -fn $fn -msg ("Error occured in vCenter Connections" );
-	throw ("Error occured in vCenter Connections" );
+	if($gotoSuccess -eq $_.Exception.Message) 
+	{
+		$fReturn = $true;
+	} 
+	else 
+	{
+		[string] $ErrorText = "catch [$($_.FullyQualifiedErrorId)]";
+		$ErrorText += (($_ | fl * -Force) | Out-String);
+		$ErrorText += (($_.Exception | fl * -Force) | Out-String);
+		$ErrorText += (Get-PSCallStack | Out-String);
+		
+		if($_.Exception -is [System.Net.WebException]) 
+		{
+			Log-Critical $fn ("[WebException] Request FAILED with Status '{0}'. [{1}]." -f $_.Status, $_);
+			Log-Debug $fn $ErrorText -fac 3;
+		}
+		else 
+		{
+			Log-Error $fn $ErrorText -fac 3;
+			if($gotoError -eq $_.Exception.Message) 
+			{
+				Log-Error $fn $e.Exception.Message;
+				$PSCmdlet.ThrowTerminatingError($e);
+			} 
+			elseif($gotoFailure -ne $_.Exception.Message) 
+			{ 
+				Write-Verbose ("$fn`n$ErrorText"); 
+			} 
+			else 
+			{
+				# N/A
+			}
+		}
+		$fReturn = $false;
+		$OutputParameter = $null;
+	}
+}
+finally 
+{
+	# Clean up
+	# N/A
 }
 
 }
 # PROCESS
 
-END
+END 
 {
+
+$datEnd = [datetime]::Now;
+Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
+
+# Return values are always and only returned via OutputParameter.
+return $OutputParameter;
+
 }
 # END
 
@@ -228,4 +402,129 @@ if($MyInvocation.ScriptName) { Export-ModuleMember -Function Enter-vCenter; }
 # aWvaQEbAzeS/RbaDXNhNpn9cDNLGPn995mm1emGG5sBmqQ6cPHEN7bHSHEoZPXM4
 # Ink/CNqF04YrB8eic4rpWbLUmVpNZsby7wm61G71kCAk9iaVZcCARkVYuh0lDmb3
 # zVGBcnD1h4LHtC4i9N7DRTCxKCMK/YEvR788FIsJ3z93huPmCfj+PqoNs2sI1q6e
+# SIG # End signature block
+
+# SIG # Begin signature block
+# MIIW3AYJKoZIhvcNAQcCoIIWzTCCFskCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUE58alPE56Bi2Z/czjINt2Ck1
+# TfmgghGYMIIEFDCCAvygAwIBAgILBAAAAAABL07hUtcwDQYJKoZIhvcNAQEFBQAw
+# VzELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNV
+# BAsTB1Jvb3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw0xMTA0
+# MTMxMDAwMDBaFw0yODAxMjgxMjAwMDBaMFIxCzAJBgNVBAYTAkJFMRkwFwYDVQQK
+# ExBHbG9iYWxTaWduIG52LXNhMSgwJgYDVQQDEx9HbG9iYWxTaWduIFRpbWVzdGFt
+# cGluZyBDQSAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlO9l
+# +LVXn6BTDTQG6wkft0cYasvwW+T/J6U00feJGr+esc0SQW5m1IGghYtkWkYvmaCN
+# d7HivFzdItdqZ9C76Mp03otPDbBS5ZBb60cO8eefnAuQZT4XljBFcm05oRc2yrmg
+# jBtPCBn2gTGtYRakYua0QJ7D/PuV9vu1LpWBmODvxevYAll4d/eq41JrUJEpxfz3
+# zZNl0mBhIvIG+zLdFlH6Dv2KMPAXCae78wSuq5DnbN96qfTvxGInX2+ZbTh0qhGL
+# 2t/HFEzphbLswn1KJo/nVrqm4M+SU4B09APsaLJgvIQgAIMboe60dAXBKY5i0Eex
+# +vBTzBj5Ljv5cH60JQIDAQABo4HlMIHiMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMB
+# Af8ECDAGAQH/AgEAMB0GA1UdDgQWBBRG2D7/3OO+/4Pm9IWbsN1q1hSpwTBHBgNV
+# HSAEQDA+MDwGBFUdIAAwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFs
+# c2lnbi5jb20vcmVwb3NpdG9yeS8wMwYDVR0fBCwwKjAooCagJIYiaHR0cDovL2Ny
+# bC5nbG9iYWxzaWduLm5ldC9yb290LmNybDAfBgNVHSMEGDAWgBRge2YaRQ2XyolQ
+# L30EzTSo//z9SzANBgkqhkiG9w0BAQUFAAOCAQEATl5WkB5GtNlJMfO7FzkoG8IW
+# 3f1B3AkFBJtvsqKa1pkuQJkAVbXqP6UgdtOGNNQXzFU6x4Lu76i6vNgGnxVQ380W
+# e1I6AtcZGv2v8Hhc4EvFGN86JB7arLipWAQCBzDbsBJe/jG+8ARI9PBw+DpeVoPP
+# PfsNvPTF7ZedudTbpSeE4zibi6c1hkQgpDttpGoLoYP9KOva7yj2zIhd+wo7AKvg
+# IeviLzVsD440RZfroveZMzV+y5qKu0VN5z+fwtmK+mWybsd+Zf/okuEsMaL3sCc2
+# SI8mbzvuTXYfecPlf5Y1vC0OzAGwjn//UYCAp5LUs0RGZIyHTxZjBzFLY7Df8zCC
+# BCgwggMQoAMCAQICCwQAAAAAAS9O4TVcMA0GCSqGSIb3DQEBBQUAMFcxCzAJBgNV
+# BAYTAkJFMRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290
+# IENBMRswGQYDVQQDExJHbG9iYWxTaWduIFJvb3QgQ0EwHhcNMTEwNDEzMTAwMDAw
+# WhcNMTkwNDEzMTAwMDAwWjBRMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFs
+# U2lnbiBudi1zYTEnMCUGA1UEAxMeR2xvYmFsU2lnbiBDb2RlU2lnbmluZyBDQSAt
+# IEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsk8U5xC+1yZyqzaX
+# 71O/QoReWNGKKPxDRm9+KERQC3VdANc8CkSeIGqk90VKN2Cjbj8S+m36tkbDaqO4
+# DCcoAlco0VD3YTlVuMPhJYZSPL8FHdezmviaJDFJ1aKp4tORqz48c+/2KfHINdAw
+# e39OkqUGj4fizvXBY2asGGkqwV67Wuhulf87gGKdmcfHL2bV/WIaglVaxvpAd47J
+# MDwb8PI1uGxZnP3p1sq0QB73BMrRZ6l046UIVNmDNTuOjCMMdbbehkqeGj4KUEk4
+# nNKokL+Y+siMKycRfir7zt6prjiTIvqm7PtcYXbDRNbMDH4vbQaAonRAu7cf9DvX
+# c1Qf8wIDAQABo4H6MIH3MA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/
+# AgEAMB0GA1UdDgQWBBQIbti2nIq/7T7Xw3RdzIAfqC9QejBHBgNVHSAEQDA+MDwG
+# BFUdIAAwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20v
+# cmVwb3NpdG9yeS8wMwYDVR0fBCwwKjAooCagJIYiaHR0cDovL2NybC5nbG9iYWxz
+# aWduLm5ldC9yb290LmNybDATBgNVHSUEDDAKBggrBgEFBQcDAzAfBgNVHSMEGDAW
+# gBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0BAQUFAAOCAQEAIlzF3T30
+# C3DY4/XnxY4JAbuxljZcWgetx6hESVEleq4NpBk7kpzPuUImuztsl+fHzhFtaJHa
+# jW3xU01UOIxh88iCdmm+gTILMcNsyZ4gClgv8Ej+fkgHqtdDWJRzVAQxqXgNO4yw
+# cME9fte9LyrD4vWPDJDca6XIvmheXW34eNK+SZUeFXgIkfs0yL6Erbzgxt0Y2/PK
+# 8HvCFDwYuAO6lT4hHj9gaXp/agOejUr58CgsMIRe7CZyQrFty2TDEozWhEtnQXyx
+# Axd4CeOtqLaWLaR+gANPiPfBa1pGFc0sGYvYcJzlLUmIYHKopBlScENe2tZGA7Bo
+# DiTvSvYLJSTvJDCCBJ8wggOHoAMCAQICEhEhQFwfDtJYiCvlTYaGuhHqRTANBgkq
+# hkiG9w0BAQUFADBSMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBu
+# di1zYTEoMCYGA1UEAxMfR2xvYmFsU2lnbiBUaW1lc3RhbXBpbmcgQ0EgLSBHMjAe
+# Fw0xMzA4MjMwMDAwMDBaFw0yNDA5MjMwMDAwMDBaMGAxCzAJBgNVBAYTAlNHMR8w
+# HQYDVQQKExZHTU8gR2xvYmFsU2lnbiBQdGUgTHRkMTAwLgYDVQQDEydHbG9iYWxT
+# aWduIFRTQSBmb3IgTVMgQXV0aGVudGljb2RlIC0gRzEwggEiMA0GCSqGSIb3DQEB
+# AQUAA4IBDwAwggEKAoIBAQCwF66i07YEMFYeWA+x7VWk1lTL2PZzOuxdXqsl/Tal
+# +oTDYUDFRrVZUjtCoi5fE2IQqVvmc9aSJbF9I+MGs4c6DkPw1wCJU6IRMVIobl1A
+# cjzyCXenSZKX1GyQoHan/bjcs53yB2AsT1iYAGvTFVTg+t3/gCxfGKaY/9Sr7KFF
+# WbIub2Jd4NkZrItXnKgmK9kXpRDSRwgacCwzi39ogCq1oV1r3Y0CAikDqnw3u7sp
+# Tj1Tk7Om+o/SWJMVTLktq4CjoyX7r/cIZLB6RA9cENdfYTeqTmvT0lMlnYJz+iz5
+# crCpGTkqUPqp0Dw6yuhb7/VfUfT5CtmXNd5qheYjBEKvAgMBAAGjggFfMIIBWzAO
+# BgNVHQ8BAf8EBAMCB4AwTAYDVR0gBEUwQzBBBgkrBgEEAaAyAR4wNDAyBggrBgEF
+# BQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20vcmVwb3NpdG9yeS8wCQYD
+# VR0TBAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDBCBgNVHR8EOzA5MDegNaAz
+# hjFodHRwOi8vY3JsLmdsb2JhbHNpZ24uY29tL2dzL2dzdGltZXN0YW1waW5nZzIu
+# Y3JsMFQGCCsGAQUFBwEBBEgwRjBEBggrBgEFBQcwAoY4aHR0cDovL3NlY3VyZS5n
+# bG9iYWxzaWduLmNvbS9jYWNlcnQvZ3N0aW1lc3RhbXBpbmdnMi5jcnQwHQYDVR0O
+# BBYEFNSihEo4Whh/uk8wUL2d1XqH1gn3MB8GA1UdIwQYMBaAFEbYPv/c477/g+b0
+# hZuw3WrWFKnBMA0GCSqGSIb3DQEBBQUAA4IBAQACMRQuWFdkQYXorxJ1PIgcw17s
+# LOmhPPW6qlMdudEpY9xDZ4bUOdrexsn/vkWF9KTXwVHqGO5AWF7me8yiQSkTOMjq
+# IRaczpCmLvumytmU30Ad+QIYK772XU+f/5pI28UFCcqAzqD53EvDI+YDj7S0r1tx
+# KWGRGBprevL9DdHNfV6Y67pwXuX06kPeNT3FFIGK2z4QXrty+qGgk6sDHMFlPJET
+# iwRdK8S5FhvMVcUM6KvnQ8mygyilUxNHqzlkuRzqNDCxdgCVIfHUPaj9oAAy126Y
+# PKacOwuDvsu4uyomjFm4ua6vJqziNKLcIQ2BCzgT90Wj49vErKFtG7flYVzXMIIE
+# rTCCA5WgAwIBAgISESFgd9/aXcgt4FtCBtsrp6UyMA0GCSqGSIb3DQEBBQUAMFEx
+# CzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMScwJQYDVQQD
+# Ex5HbG9iYWxTaWduIENvZGVTaWduaW5nIENBIC0gRzIwHhcNMTIwNjA4MDcyNDEx
+# WhcNMTUwNzEyMTAzNDA0WjB6MQswCQYDVQQGEwJERTEbMBkGA1UECBMSU2NobGVz
+# d2lnLUhvbHN0ZWluMRAwDgYDVQQHEwdJdHplaG9lMR0wGwYDVQQKDBRkLWZlbnMg
+# R21iSCAmIENvLiBLRzEdMBsGA1UEAwwUZC1mZW5zIEdtYkggJiBDby4gS0cwggEi
+# MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDTG4okWyOURuYYwTbGGokj+lvB
+# go0dwNYJe7HZ9wrDUUB+MsPTTZL82O2INMHpQ8/QEMs87aalzHz2wtYN1dUIBUae
+# dV7TZVme4ycjCfi5rlL+p44/vhNVnd1IbF/pxu7yOwkAwn/iR+FWbfAyFoCThJYk
+# 9agPV0CzzFFBLcEtErPJIvrHq94tbRJTqH9sypQfrEToe5kBWkDYfid7U0rUkH/m
+# bff/Tv87fd0mJkCfOL6H7/qCiYF20R23Kyw7D2f2hy9zTcdgzKVSPw41WTsQtB3i
+# 05qwEZ3QCgunKfDSCtldL7HTdW+cfXQ2IHItN6zHpUAYxWwoyWLOcWcS69InAgMB
+# AAGjggFUMIIBUDAOBgNVHQ8BAf8EBAMCB4AwTAYDVR0gBEUwQzBBBgkrBgEEAaAy
+# ATIwNDAyBggrBgEFBQcCARYmaHR0cHM6Ly93d3cuZ2xvYmFsc2lnbi5jb20vcmVw
+# b3NpdG9yeS8wCQYDVR0TBAIwADATBgNVHSUEDDAKBggrBgEFBQcDAzA+BgNVHR8E
+# NzA1MDOgMaAvhi1odHRwOi8vY3JsLmdsb2JhbHNpZ24uY29tL2dzL2dzY29kZXNp
+# Z25nMi5jcmwwUAYIKwYBBQUHAQEERDBCMEAGCCsGAQUFBzAChjRodHRwOi8vc2Vj
+# dXJlLmdsb2JhbHNpZ24uY29tL2NhY2VydC9nc2NvZGVzaWduZzIuY3J0MB0GA1Ud
+# DgQWBBTwJ4K6WNfB5ea1nIQDH5+tzfFAujAfBgNVHSMEGDAWgBQIbti2nIq/7T7X
+# w3RdzIAfqC9QejANBgkqhkiG9w0BAQUFAAOCAQEAB3ZotjKh87o7xxzmXjgiYxHl
+# +L9tmF9nuj/SSXfDEXmnhGzkl1fHREpyXSVgBHZAXqPKnlmAMAWj0+Tm5yATKvV6
+# 82HlCQi+nZjG3tIhuTUbLdu35bss50U44zNDqr+4wEPwzuFMUnYF2hFbYzxZMEAX
+# Vlnaj+CqtMF6P/SZNxFvaAgnEY1QvIXI2pYVz3RhD4VdDPmMFv0P9iQ+npC1pmNL
+# mCaG7zpffUFvZDuX6xUlzvOi0nrTo9M5F2w7LbWSzZXedam6DMG0nR1Xcx0qy9wY
+# nq4NsytwPbUy+apmZVSalSvldiNDAfmdKP0SCjyVwk92xgNxYFwITJuNQIto4zGC
+# BK4wggSqAgEBMGcwUTELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24g
+# bnYtc2ExJzAlBgNVBAMTHkdsb2JhbFNpZ24gQ29kZVNpZ25pbmcgQ0EgLSBHMgIS
+# ESFgd9/aXcgt4FtCBtsrp6UyMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQow
+# CKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcC
+# AQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQrGgz6dIpWQz+3MZ8y
+# 6MdZ+8i8TTANBgkqhkiG9w0BAQEFAASCAQBGQu2SbwdLUI3iVJu4FIbMCDS1KNDh
+# I5D0wFQrjMjOVgrqe9xggDViZ6z5X0OzRS52JtSDupjqYdy6RnXzg5ILvwgg5j1u
+# nytGVwhTe4ALljBMYmqyqWB1VEAFXXitm4wZm8amRixy0Px6ScGgcE+PeIO6RLYK
+# k+0YU3V2C+OGFCI4ReH1oXlM93ZkPCPkIcNm6GXWWwbF5owf3ET71iHO1UC0Qx5U
+# 77ICuLOaASFy31RnV3VnsP+5ajPuXnU/fp2ExlxfAY4a7WRvdYqfo/8LPVB+9hfQ
+# doREFy3/waxAJVMOyZ9K86nnkBA0CVYj7jRfUcFiy45WIPlaFH2KpdsroYICojCC
+# Ap4GCSqGSIb3DQEJBjGCAo8wggKLAgEBMGgwUjELMAkGA1UEBhMCQkUxGTAXBgNV
+# BAoTEEdsb2JhbFNpZ24gbnYtc2ExKDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0
+# YW1waW5nIENBIC0gRzICEhEhQFwfDtJYiCvlTYaGuhHqRTAJBgUrDgMCGgUAoIH9
+# MBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDEy
+# MDExMzY1NVowIwYJKoZIhvcNAQkEMRYEFPDGzb41+Es/Dekm7I4tcmXuWRieMIGd
+# BgsqhkiG9w0BCRACDDGBjTCBijCBhzCBhAQUjOafUBLh0aj7OV4uMeK0K947NDsw
+# bDBWpFQwUjELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2Ex
+# KDAmBgNVBAMTH0dsb2JhbFNpZ24gVGltZXN0YW1waW5nIENBIC0gRzICEhEhQFwf
+# DtJYiCvlTYaGuhHqRTANBgkqhkiG9w0BAQEFAASCAQBuM2ZAaFsuxnUGlXpHZCcW
+# u+8y4Jr5hYZluEX4xfPqb3pPSREWoGhBWNw1AAwqnNno22vRLLyLhwuc3NJTGvAj
+# FkOjH2JFH+gcN3r9a/CFDuZqmWbz95yUnH0YkBXuwjU2ookZgGkEtsAIAqCep+IL
+# vzgSe4fX+U/wq+XymZ/oOySowwWbzlueV2fk7T8sMqonQusnMN7qK4MIILs02ikl
+# 0u8Vx7p+oPimLe99F8TOZrjvY+nnq9VHvzfnkrBfV51FaDfHgxgfSjtRsivMf5uV
+# 6yqguxULGjjrRaam1lWECi5yvnWyWlpNMdVJwPqULnc2vnbvsyw7qOFcJZQHdmH6
 # SIG # End signature block
